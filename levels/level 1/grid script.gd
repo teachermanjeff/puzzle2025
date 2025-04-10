811a08ca5b2afbe5ca88b9c4a39438020484850a
KhanakKRDS <2652104@sd61learn.ca>
2025-04-10 12:01:38 -0700

    added a script for the grid
    have some errors but soon will work. this code is for arranging gems randomly

---
+++ levels/level 1/grid script.gd
@@ -0,0 +1,44 @@
+extends Node2D
+
+@export var width: int
+@export var height: int
+@export var x_start: int
+@export var y_start: int
+@export var offset: int
+
+var all_gems = []
+var gems = [
+	preload("res://blue_gem.tscn"),
+	preload("res://light_green_gem.tscn"),
+	preload("res://yellow_gem.tscn"),
+	preload("res://red_gem.tscn"),
+	preload("res://purple_gem.tscn")
+]
+
+func _ready():
+	randomize()
+	all_gems = make_2D_array()
+	spawn_gems()
+
+func make_2D_array() -> Array:
+	var array = []
+	for i in range(width): 
+		array.append([])
+		for j in range(height):  
+			array[i].append(null)
+	return array
+
+func spawn_gems():
+	for j in range(height):  
+		for i in range(width): # added loop over width
+			var rand = floor(rand_range(0, gems.size()))
+			var piece = gems[rand].instantiate()
+			add_child(piece)	
+			piece.position = grid_to_pixel(i, j)
+
+func grid_to_pixel(column, row):
+	var new_x = x_start + offset * column
+	var new_y = y_start + -offset * row
+	return Vector2(new_x, new_y)
+
+	
