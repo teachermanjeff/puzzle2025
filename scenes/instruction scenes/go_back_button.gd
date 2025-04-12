8890d98a114a3a1ec921ee35bec10041d55e7b5e
KhanakKRDS <2652104@sd61learn.ca>
2025-04-12 14:35:48 -0700

    Start/ instruction screen
    Rewrote the script for both the screens and all buttons are working.

--- scenes/instruction scenes/go_back_button.gd
+++ scenes/instruction scenes/go_back_button.gd
@@ -1,4 +1,4 @@
 extends Button
 
 func _on_button_down():
-	pass
+	get_tree().change_scene_to_file("res://scenes/start scene/Start Screen.tscn")
---
+++ scenes/start scene/start_button.gd
@@ -0,0 +1,4 @@
+extends Button
+
+func _on_button_down():
+	get_tree().change_scene_to_file("res://scenes/levels/level 1/level1.tscn")
