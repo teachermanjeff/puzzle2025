cfe43ff4bb1a72fb4268a4007342e8d526753a7f
zara <131491949+meisnotavailabe@users.noreply.github.com>
2025-04-17 11:01:33 -0700

    fixed menu
    c

--- scenes/menu scene/failscreen.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-
-func _on_instruct_but_button_down() -> void:
-	get_tree().change_scene_to_file("res://scenes/instructionscreen.tscn")
-
-
-func _on_contd_button_down() -> void:
-	get_tree().change_scene_to_file("res://scenes/start scene/Start Screen.tscn")
