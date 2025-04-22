ced652cdf873268636005c7642c31e02d63caf1c
KhanakKRDS <2652104@sd61learn.ca>
2025-04-22 11:59:30 -0700

    power-up script
    Added a power-up script with a timer. The box added is temporary, I will change it with Bella's animation. The box acts as a yay! sign, appears if randomly gets a power-up

--- scenes/levels/level 1/power_up_1.gd
+++ scenes/levels/level 1/power_up_1.gd
@@ -4,13 +4,14 @@ var timer = Timer
 var object_to_appear =Node
 
 func _ready() -> void:
-	timer = $"power-up Timer 2"
-	timer.wait_time = 60 # 60sec timer
+	timer = $"power-up Timer 1"
+	timer.wait_time = 30 # 30sec timer
 	timer.one_shot = false #repeat time
 	timer.start()
 	
-	object_to_appear = $"power-up (rand) bg 2"
+	object_to_appear = $"power-up (rand) bg 1"
 	object_to_appear.visible = false #in start invisible 
 	
-func _on_powerup_timer_2_timeout() -> void:
+func _on_powerup_timer_1_timeout() -> void:
 	object_to_appear.visible = true 
+	
--- scenes/levels/level 3/power_up_3.gd
+++ scenes/levels/level 3/power_up_3.gd
@@ -5,12 +5,12 @@ var object_to_appear =Node
 
 func _ready() -> void:
 	timer = $"power-up Timer 2"
-	timer.wait_time = 60 # 60sec timer
+	timer.wait_time = 120 # 120sec timer
 	timer.one_shot = false #repeat time
 	timer.start()
 	
 	object_to_appear = $"power-up (rand) bg 2"
 	object_to_appear.visible = false #in start invisible 
-	
-func _on_powerup_timer_2_timeout() -> void:
+
+func _on_powerup_timer_3_timeout() -> void:
 	object_to_appear.visible = true 
