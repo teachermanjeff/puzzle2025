03cf71f80f12eb3222504c06a4adf9b3c8a2f2b0
KhanakKRDS <2652104@sd61learn.ca>
2025-04-23 11:06:40 -0700

    updated version

--- scenes/levels/level 1/power_up_1.gd
+++ scenes/levels/level 1/power_up_1.gd
@@ -3,13 +3,17 @@ extends Node2D
 #variables
 var timer = Timer
 var disappear_timer = Timer
-var object_to_appear =Node
+var object_to_appear = Node
+var extra_moves = Node
+@onready var movements_cd_l_1: Label = $"../Moves left 1/Movements-CD-L1"
 
 func _ready() -> void:
 	#the time it is visible for
 	timer = $"power-up Timer 1 (appear)"
 	timer.wait_time = 30 # 30sec timer
 	timer.one_shot = false #repeat time
+	movements_cd_l_1.attempts_left += 5
+	print("extra_moves",extra_moves)
 	timer.start()
 	
 	#the time it is not visible for
