65377728a44c05381a8e0888c3e5a7ea9a4175b3
KhanakKRDS <2652104@sd61learn.ca>
2025-04-22 19:28:42 -0700

    updated power-up
    but the error is still the same

--- scenes/levels/level 1/power_up_1.gd
+++ scenes/levels/level 1/power_up_1.gd
@@ -1,18 +1,21 @@
 extends Node2D
 
-var timer = Timer
-var disappear_timer = Timer
+#variables
+var timer = Time
+var disappear_timer = Time
 var object_to_appear =Node
 
 func _ready() -> void:
+	#the time it is visible for
 	timer = $"power-up Timer 1 (appear)"
 	timer.wait_time = 30 # 30sec timer
 	timer.one_shot = false #repeat time
 	timer.start()
 	
-	disappear_timer = $"power-up Timer1 (disappear)"
+	#the time it is not visible for
+	disappear_timer = $"power-up Timer 1 (disappear)"
 	disappear_timer.wait_time = 10 #10sec timer
-	disappear_timer.one_shot = false
+	disappear_timer.one_shot = false #repeat timer
 	
 	object_to_appear = $"power-up (rand) bg 1"
 	object_to_appear.visible = false #in start invisible 
--- scenes/levels/level 2/power_up_2.gd
+++ scenes/levels/level 2/power_up_2.gd
@@ -1,18 +1,21 @@
 extends Node2D
 
-var timer = Timer
+#variables
+var timer = Time
 var disappear_timer = Time
 var object_to_appear =Node
 
 func _ready() -> void:
+	#the time it is visible for
 	timer = $"power-up Timer 2 (appear)"
 	timer.wait_time = 60 # 60sec timer
 	timer.one_shot = false #repeat time
 	timer.start()
 	
+	#the time it is not visible for
 	disappear_timer = $"power-up Timer 2 (disappear)"
 	disappear_timer.wait_time = 10 #10sec timer
-	disappear_timer.one_shot = false
+	disappear_timer.one_shot = false #repeat time
 	
 	object_to_appear = $"power-up (rand) bg 2"
 	object_to_appear.visible = false #in start invisible 
