14fc352acb7a43f9e17db8c5e8076f1b213b3731
KhanakKRDS <2652104@sd61learn.ca>
2025-05-05 21:49:18 -0700

    Power-up
    The character is added and the power-up works for all the levels.

--- scenes/levels/level 2/power_up_2.gd
+++ scenes/levels/level 2/power_up_2.gd
@@ -5,7 +5,7 @@ var timer = Timer
 var disappear_timer = Timer
 var object_to_appear = Node
 var extra_moves = Node
-@onready var movements_cd_l_2: Label = $"../movements-CD-L2"
+@onready var moves_left_2 = $"../Moves left 2"
 
 func _ready() -> void:
 	#the time it is visible for
@@ -19,13 +19,13 @@ func _ready() -> void:
 	disappear_timer.wait_time = 10 #10sec timer
 	disappear_timer.one_shot = false #repeat timer
 	
-	object_to_appear = $"power-up (rand) bg 2"
+	object_to_appear = $"character_power-up_animation"
 	object_to_appear.visible = false #in start invisible 
 	
 func _on_powerup_timer_2_appear_timeout():
 	object_to_appear.visible = true
 	disappear_timer.start()
-	movements_cd_l_2.attempts_left += 5
+	moves_left_2.attempts_left += 5
 	print("extra_moves",extra_moves)
 
 func _on_powerup_timer_2_disappear_timeout():
--- scenes/levels/level 3/power_up_3.gd
+++ scenes/levels/level 3/power_up_3.gd
@@ -5,7 +5,7 @@ var timer = Timer
 var disappear_timer = Timer
 var object_to_appear = Node
 var extra_moves = Node
-@onready var movements_cd_l_3: Label = $"../Moves left 3/movements-CD-L3"
+@onready var movements_cd_l_3 = $"../Movements-CD-L3"
 
 func _ready() -> void:
 	#the time it is visible for
@@ -19,7 +19,7 @@ func _ready() -> void:
 	disappear_timer.wait_time = 10 #10sec timer
 	disappear_timer.one_shot = false #repeat timer
 	
-	object_to_appear = $"power-up (rand) bg 3"
+	object_to_appear = $"character_power-up_animation"
 	object_to_appear.visible = false #in start invisible 
 	
 func _on_powerup_timer_3_appear_timeout():
