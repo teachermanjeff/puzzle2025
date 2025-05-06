d5e0eb57b06b291cbfa912d796897b502d056e5a
KhanakKRDS <2652104@sd61learn.ca>
2025-05-06 12:27:11 -0700

    fixed error

--- scenes/levels/movements_countdown.gd
+++ scenes/levels/movements_countdown.gd
@@ -3,40 +3,38 @@ extends Label
 # Variables for the movement countdown and points
 @export var max_attempts = 30  # Maximum number of attempts
 var attempts_left # How many attempts are left
-@onready var score: Label = $"../grid script/score"
+@onready var score: Label = $"../grid 1 script/score"
 
 func _ready():
 	# Optionally, set up your UI to show countdown and points
 	attempts_left = max_attempts
 	update_ui()
 	
-	print(get_tree().current_scene.name)
-
+	
 func _process(delta: float) -> void:
 	update_ui()
 
 # Function to update the UI (this example prints to console)
 func update_ui():
 	text = str(attempts_left)
-	failed()
-	win()
-	
+
+
 func _on_grid_1_script_gemswap() -> void:
 	attempts_left -= 1
+
 
 func _on_grid_2_script_gemswap() -> void:
 	attempts_left -= 1
 
+
 func _on_grid_3_script_gemswap() -> void:
 	attempts_left -= 1
 	
-func failed():
+func _failed():
 	if attempts_left == 0:
 		get_tree().change_scene_to_file("res://scenes/menu scene/failscreen(1).tscn")
-
 func win():		
 	if score.score == 80:
-		print("reached 80 score")
-		globals.wonscene = str(get_tree().current_scene.name)
+		globals.wonscene = get_tree().current_scene
 		get_tree().change_scene_to_file("res://scenes/menu scene/failscreen(1).tscn")
 	
--- scenes/menu scene/winish.gd
+++ scenes/menu scene/winish.gd
@@ -1,11 +1,11 @@
 extends CanvasLayer
 
 func _on_contd_pressed() -> void:
-	if globals.wonscene == "level 1":
+	if globals.wonscene == "level1":
 		get_tree().change_scene_to_file("res://scenes/levels/level 2/level_2.tscn")
-	if globals.wonscene == "level 2":
+	if globals.wonscene == "level2":
 		get_tree().change_scene_to_file("res://scenes/levels/level 3/level_3.tscn")
-	if globals.wonscene == "level 3":
+	if globals.wonscene == "level3":
 		get_tree().change_scene_to_file("res://winscreen.tscn")
 
 func _on_instruct_but_2_pressed() -> void:
